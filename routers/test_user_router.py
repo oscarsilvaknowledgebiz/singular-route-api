69b1ccde2d8e0511cae1074bcd36c71d1dae8ff4
Daniel Costa <daniel.costa@knowledgebiz.pt>
2023-05-09 09:53:37 +0100

    Task 106 - Login

--- routers/test_user_router.py
+++ routers/test_user_router.py
@@ -6,5 +6,5 @@ import requests
 def test_fetch_user_id():
     second_validate = internal.validate_uuid.is_uuid_valid("123")
     validate = internal.validate_uuid.is_uuid_valid("1f58ff58-daa1-4e35-8910-5293d7667d40")
-    assert validate == True
-    assert second_validate == False
+    assert validate is True
+    assert second_validate is False
